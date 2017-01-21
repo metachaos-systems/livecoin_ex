03927b4fff53f959d50d30cd3779ab007357a085
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-01-21 14:46:51 +0200

    last trades test red

--- test/livecoin_ex_test.exs
+++ test/livecoin_ex_test.exs
@@ -2,6 +2,10 @@ defmodule LivecoinExTest do
   use ExUnit.Case
   doctest LivecoinEx
 
+  test "last trades" do
+    assert {:ok,  [%{ "time": _, "id": _, "price": _, "quantity": _, "type": "BUY" } | _ ]} = LivecoinEx.last_trades()
+  end
+
   test "ticker for all pairs" do
     assert {:ok,  [%{"best_ask" => _, "best_bid" => _, "vwap" => _} | _ ]} = LivecoinEx.ticker()
   end
